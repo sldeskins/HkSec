0e28dbba83053740e058aa50c7acb37ba74ce790
sldeskins <sldeskins@gmail.com>
2015-07-23 11:33:48 -0400

    start example with Http module , setup webconfig servers to user it with Helper class to write to output window while going through the chain; Add the TestController with StartUp Class, add routest; add middleware and add to Startup

---
+++ X509Examples/SecuirtyPipeline/Pipeline/TestController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Net.Http;
+
+namespace SecurityPipeline.Pipeline
+{
+    public class TestController:ApiController
+
+    {
+        public IHttpActionResult Get () { 
+        Helper.Write("Controller", User);
+
+        Helper.Write("Controller - alternative", Request.GetRequestContext().Principal);
+    
+
+
+
+        return Ok();
+        }
+    }
+}
---
+++ X509Examples/SecuirtyPipeline/Pipeline/TestMiddleware.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace SecurityPipeline.Pipeline
+{
+    public class TestMiddleware
+    {
+        private Func<IDictionary<string, object>, Task> _next;
+        public TestMiddleware ( Func<IDictionary<string, object>, Task> next )
+        {
+            _next = next;
+        }
+
+        public async Task Invoke ( IDictionary<string, object> env )
+        {
+            //"server.user"
+            var context = new OwinContext(env);
+
+            Helper.Write("Middleware", context.Request.User);
+
+            await _next(env);
+        }
+    }
+}
