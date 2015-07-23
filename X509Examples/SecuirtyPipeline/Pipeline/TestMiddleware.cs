dd1322f6acfd9ce287389fc5b6b5b730d275cb2a
sldeskins <sldeskins@gmail.com>
2015-07-23 13:17:07 -0400

    middleware katana like authentican added "dom"

--- X509Examples/SecuirtyPipeline/Pipeline/TestMiddleware.cs
+++ X509Examples/SecuirtyPipeline/Pipeline/TestMiddleware.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Threading.Tasks;
 using System.Web;
 
@@ -19,6 +20,11 @@ namespace SecurityPipeline.Pipeline
         {
             //"server.user"
             var context = new OwinContext(env);
+
+
+            //authentication
+            context.Request.User = new GenericPrincipal(new GenericIdentity("dom"), new string[]{});
+
 
             Helper.Write("Middleware", context.Request.User);
 
